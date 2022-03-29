14c17675b251cef59c4ae8228a6d0c37ec622e15
Hat <nestorsuat@gmail.com>
2022-03-29 16:04:04 -0500

    Implementando Nodejs
    Aquí he creado la primera dependencia en NodeJs lista para ser ejecutada y lanzada en el navegador.
    Archivos modificados:
    - index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var hello = "Hola mundo"
-let number = 3 + 7
+const http = require('http')
 
-console.log(hello + " " + number)
+const hostname = '127.0.0.1'
+const port = 3000
+
+var hello = 'Hello'
+let number = 3
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(hello+ ': Hola mundo '+number)
+})
+
+server.listen(port, hostname, () =>{
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`)
+})
