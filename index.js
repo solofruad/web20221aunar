845e4154cbffef2c820c4b1804e4917d717486e2
Hat <nestorsuat@gmail.com>
2022-03-29 16:11:51 -0500

    Iniciando dependencia con express
    Se ha modificado el archivo y se comienza a usar express js como framework para trabajar con nodejs.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const http = require('http')
-
-const hostname = '127.0.0.1'
+const express = require('express')
+const app = express()
 const port = 3000
 
 var hello = 'Hello'
 let number = 3
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(hello+ ': Hola mundo '+number)
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
 })
-
-server.listen(port, hostname, () =>{
-    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`)
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
 })
